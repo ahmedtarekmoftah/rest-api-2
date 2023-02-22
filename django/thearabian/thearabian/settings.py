142de85aa1ffc3c82cee46d673e7526c73c16b4e
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-22 11:04:37 +0200

    s

--- django/thearabian/thearabian/settings.py
+++ django/thearabian/thearabian/settings.py
@@ -106,14 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
 
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
-        'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.IsAdminUser',
-    ),
-}
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
