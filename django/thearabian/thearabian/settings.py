a4b9a731392b1e08003d388d4ed7f09bcc1d7280
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-22 10:55:24 +0200

    Update settings.py

--- django/thearabian/thearabian/settings.py
+++ django/thearabian/thearabian/settings.py
@@ -107,12 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
 
-        'rest_framework.permissions.IsAuthenticated'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
+    ),
 }
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
