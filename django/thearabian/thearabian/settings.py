ceba380ae05f34a97aed22b4d1eec3fd77506cc1
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-22 10:58:44 +0200

    Update settings.py

--- django/thearabian/thearabian/settings.py
+++ django/thearabian/thearabian/settings.py
@@ -110,8 +110,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-        'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.IsAdminUser',
+
     ),
 }
 # Internationalization
