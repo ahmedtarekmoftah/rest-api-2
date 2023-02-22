7ff2aba7b67401738374eff80ee472aafdc10d8c
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-22 08:47:10 +0200

    Update models.py

--- django/thearabian/rest/models.py
+++ django/thearabian/rest/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class country(models.Model):
-    name = model.CharField(max_length=255, default="قريبا")
-    title = model.CharField(max_lenght=255, default="قريبا")
+    name = models.CharField(max_length=255, default="قريبا")
+    title = models.CharField(max_lenght=255, default="قريبا")
 
     def __str__(self):
         return f"{self.id}. {self.name}"
