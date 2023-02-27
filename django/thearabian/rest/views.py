4026a612218d73950065288d676432f8dee2270e
ahmedtarekmoftah <67566992+ahmedtarekmoftah@users.noreply.github.com>
2023-02-27 13:20:14 +0200

    Update views.py

--- django/thearabian/rest/views.py
+++ django/thearabian/rest/views.py
@@ -21,8 +21,9 @@ class CountryListCreateAPIView(generics.ListCreateAPIView):
     queryset = country.objects.all()
     serializer_class = CountrySerializer
     permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [authentication.SessionAuthentication,
-                              authentication.TokenAuthentication]
+    authentication_classes = [
+        authentication.JWTAuthentication, authentication.SessionAuthentication,
+        authentication.TokenAuthentication]
 
 
 country_list_create_view = CountryListCreateAPIView.as_view()
